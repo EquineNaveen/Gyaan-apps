b16a535f879ca2671686dd9f246b31b3297ad702
EquineNaveen <snaveengowda13063@gmail.com>
2025-04-26 00:17:46 +0530

    redirecting working correctly

--- login.py
+++ login.py
@@ -2,19 +2,19 @@ import streamlit as st
 from ldap3 import Server, Connection, ALL, NTLM, SIMPLE, ALL_ATTRIBUTES, SUBTREE, core
 
 def ldap_authenticate(username: str, password: str):
-    server_uri = "ldap://10.21.6.164:389"
-    base_dn = "dc=dos"
-    user_dn = f"mailacceptinggeneralid={username},{base_dn}"
-    try:
-        server = Server(server_uri, get_info=ALL)
-        conn = Connection(server, user=user_dn, password=password, authentication=SIMPLE, auto_bind=True)
-        conn.unbind()
-        return True
-    except core.exceptions.LDAPBindError:
-        return False
-    except Exception as error:
-        st.error(f"LDAP error: {error}")
-        return False
+    # server_uri = "ldap://10.21.6.164:389"
+    # base_dn = "dc=dos"
+    # user_dn = f"mailacceptinggeneralid={username},{base_dn}"
+    # try:
+    #     server = Server(server_uri, get_info=ALL)
+    #     conn = Connection(server, user=user_dn, password=password, authentication=SIMPLE, auto_bind=True)
+    #     conn.unbind()
+    return True
+    # except core.exceptions.LDAPBindError:
+    #     return False
+    # except Exception as error:
+    #     st.error(f"LDAP error: {error}")
+    #     return False
 
 def login():
     st.title("Login Page")
@@ -27,14 +27,14 @@ def login():
             st.session_state['user'] = username
             st.session_state['role'] = "USER"
             st.success("Login successful!")
-            st.experimental_rerun()
+            # Redirect to apps.py after successful login
+            st.switch_page("pages/apps.py")
         else:
             st.error("Invalid credentials.")
 
 if 'user' in st.session_state:
-    # Redirect to apps.py after successful login
-    st.experimental_set_query_params(page="apps")
-    st.write("Redirecting to apps...")
+    # Redirect to apps.py if already logged in
+    st.switch_page("pages/apps.py")
     st.stop()
 else:
     login()
